0f839d4f979fdf0af36fd46aa86a438c6fece69d
Fana-Dzi <adjieschoolsmp@gmail.com>
2025-10-17 18:05:56 +0700

    pertemuan 4

---
+++ Pertemuan_4/Larapress/resources/views/kontak.blade.php
@@ -0,0 +1,22 @@
+<?php
+// kontak.php
+?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Kontak</title>
+</head>
+<body>
+    <h1>Kontak Kami</h1>
+    <p>Email: adjieschoolsmp@gmail.com</p>
+    <p>Telepon: +62 851 8077 4366</p>
+
+    <nav>
+        <a href="/">Home</a> |
+        <a href="kontak.php">Kontak</a>
+    </nav>
+
+
+</body>
+</html>
